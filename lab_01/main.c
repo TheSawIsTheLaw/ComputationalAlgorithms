286e917e92dd1e77295e25ecf17f740ef653d0f1
xAMATEURx <dimakrok@bk.ru>
2020-02-16 00:46:54 +0300

    Interpolation, Part 1

--- lab_01/main.c
+++ lab_01/main.c
@@ -14,16 +14,207 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 #define FILE_NAME "table.txt"
 #define ERROR 666
+#define EPS 0.000001
 
+//#define DEBUG
 
-int readToTable(){
+typedef struct{
+    int dotsNum;
+    double *xArgs;
+    double *yArgs;
+}tableT;
+
+// Функция чтения таблицы заданного формата
+int readfromTable(FILE *tableFile, tableT *table){
+    if (!tableFile)
+        return ERROR;
+
+    int check, dotsNum;
+    check = fscanf(tableFile, "number of dots: %d", &dotsNum);
+
+    if (!check || dotsNum <= 0)
+        return ERROR;
+
+    table->dotsNum = dotsNum;
+#ifdef DEBUG
+    printf("%d", table->dotsNum);
+#endif
+
+    table->xArgs = calloc(dotsNum, sizeof(double));
+    table->yArgs = calloc(dotsNum, sizeof(double));
+
+    for (int i = 0; i < dotsNum; i++)
+        check = fscanf(tableFile, "%lf%lf", table->xArgs + i, table->yArgs + i);
+
+    if (check != 2)
+        return ERROR;
+
     return 0;
 }
 
-int interpolationAlg(){
+int printTableT(tableT table)
+{
+#ifdef DEBUG
+    printf("%d", table.dotsNum);
+#endif
+
+    for (int i = 0; i < table.dotsNum; i++)
+        printf("|%+5lf|%+5lf|\n", *(table.xArgs + i), *(table.yArgs + i));
+
+    return 0;
+}
+
+void freeTableT(tableT *table)
+{
+    if (!table)
+        return ;
+    table->dotsNum = 0;
+    if (table->xArgs)
+        free(table->xArgs);
+    if (table->yArgs)
+        free(table->yArgs);
+}
+
+double splitDiff(double yi, double yj, double xi, double xj){
+#ifdef DEBUG
+    printf("\nyi=%lf yj=%lf xi=%lf xj=%lf\n", yi, yj,
+            xi, xj);
+#endif
+    return (yi - yj)/(xi - xj);
+}
+
+void slowSortTable(tableT *table)
+{
+    double memoryVar;
+    for (int i = 0; i < table->dotsNum; i++){
+        for (int j = 0; j < table->dotsNum - 1; j++){
+            if (table->xArgs[j] > table->xArgs[j + 1]){
+                memoryVar = table->xArgs[j];
+                table->xArgs[j] = table->xArgs[j + 1];
+                table->xArgs[j + 1] = memoryVar;
+
+                memoryVar = table->yArgs[j];
+                table->yArgs[j] = table->yArgs[j + 1];
+                table->yArgs[j + 1] = memoryVar;
+            }
+        }
+    }
+}
+
+int interpolationAlg(tableT *table, double findX, int polynomDegree){
+    if (polynomDegree <= 0)
+        return ERROR;
+    slowSortTable(table);
+
+    printf("\nОтсортированная таблица значений будет иметь следующий вид:\n");
+    printf("|%5s    |%5s    |\n", "x", "y");
+    printf("|---------|---------|\n");
+    printTableT(*table);
+    printf("\n\n");
+
+    // Нахождение позиции значения х в исходной таблице
+    int position = -1;
+    for (int i = 0; i < table->dotsNum - 1; i++){
+        if (table->xArgs[i] - findX <= EPS && table->xArgs[i + 1] - findX >= EPS){
+            position = i;
+            break;
+        }
+    }
+
+    if (position == -1){
+        printf("К сожалению, экстраполяция запрещена.\n");
+        return ERROR;
+    }
+
+#ifdef DEBUG
+    printf("pos is %d\n", position);
+#endif
+
+    // Выделение узлов
+    if (table->dotsNum < polynomDegree + 1)
+        return ERROR;
+
+    int disDegree = polynomDegree, leftMove = 0, rightMove = 0;
+    double *nodesXMas = calloc(polynomDegree + 1, sizeof(double));
+    double *nodesYMas = calloc(polynomDegree + 1, sizeof(double));
+    while (disDegree > 0){
+        if (position - leftMove > 0){
+            leftMove++;
+            disDegree--;
+        }
+        if (position + rightMove < table->dotsNum - 1){
+            rightMove++;
+            disDegree--;
+        }
+    }
+    for (int i = position - leftMove, curNode = 0; i <= position + rightMove; i++, curNode++){
+        nodesYMas[curNode] = table->yArgs[i];
+        nodesXMas[curNode] = table->xArgs[i];
+    }
+    printf("Работа будет производиться со следующими узлами:\n");
+    for (int i = 0; i < polynomDegree + 1; i++){
+        printf("x%d = %+lf; y%d = %+lf\n", i, nodesXMas[i], i, nodesYMas[i]);
+    }
+
+    double **difMas = calloc(polynomDegree + 2, sizeof(double *));
+    difMas[0] = nodesXMas;
+    difMas[1] = nodesYMas;
+    nodesXMas = NULL;
+    nodesYMas = NULL;
+    // Расчёт таблицы с разделёнными разностями
+    for (int i = polynomDegree, curPos = 2; i > 0; i--, curPos++){
+        difMas[curPos] = calloc(i, sizeof(double));
+        for (int j = 0; j < i; j++)
+            difMas[curPos][j] = splitDiff(difMas[curPos - 1][j], difMas[curPos - 1][j + 1],
+                                          difMas[0][j], difMas[0][j - 1 + curPos]);
+    }
+
+    printf("\nПолученная таблица:\n");
+    for (int i = 0; i < polynomDegree + 1; i++){
+        for (int j = 0; j < polynomDegree + 2 - i; j++)
+            printf("%+3.3f ", difMas[j][i]);
+        printf("\n");
+    }
+    printf("\n\n");
+    for (int i = 0; i < polynomDegree + 1; i++){
+        for (int j = 0; j < polynomDegree + 2 - i; j++)
+            if (difMas[j][i] == INFINITY || difMas[j][i] == -INFINITY){
+                printf("К несчастью, возникло деление на ноль. Завершение программы...\n\n");
+                for (int i = 0; i < polynomDegree + 2; i++)
+                    free(difMas[i]);
+                free(difMas);
+                return ERROR;
+            }
+    }
+
+    // Расчёт полинома
+    double finalPolynom = 1, compGap = 1;
+    printf("Слагаемые полинома:\n1\n");
+    for (int i = 0; i < polynomDegree; i++)
+    {
+        for (int j = 0; j <= i; j++){
+            printf("(x - %lf)*", difMas[0][j]);
+            compGap *= findX - difMas[0][j];
+        }
+        printf("(%lf)\n", difMas[i + 2][0]);
+        compGap *= difMas[i + 2][0];
+        finalPolynom += compGap;
+        compGap = 1;
+    }
+
+    // Удивительно... Но оно работает...
+
+    printf("\n\nPn(x) = %lf\n\n", finalPolynom);
+
+    for (int i = 0; i < polynomDegree + 2; i++)
+        free(difMas[i]);
+    free(difMas);
+
     return 0;
 }
 
@@ -42,15 +233,38 @@ int main(){
         return ERROR;
     }
 
-    readToTable();
+    tableT table = { 0 };
 
-    interpolationAlg();
+    readfromTable(tableFile, &table);
+
+    printf("Принятая таблица:\n|%5s    |%5s    |\n", "x", "y");
+    printf("|---------|---------|\n");
+    printTableT(table);
+
+    fclose(tableFile);
+
+    printf("Будьте добры, введите x: ");
+    double findX;
+    int check = scanf("%lf", &findX);
+
+    if (check != 1){
+        return ERROR;
+    }
+    printf("Будьте добры, введите степень полинома(n): ");
+    int polynomDegree;
+    check = scanf("%d", &polynomDegree);
+
+    if (check != 1 || polynomDegree <= 0){
+        return ERROR;
+    }
+
+    interpolationAlg(&table, findX, polynomDegree);
 
     halfAlg();
 
     reverseInterpolationAlg();
 
-    fclose(tableFile);
+    freeTableT(&table);
 
     return 0;
 }
