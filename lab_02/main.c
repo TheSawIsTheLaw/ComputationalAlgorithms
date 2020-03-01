1400c18172968d777f595d9b46f0a545696c89b7
xAMATEURx <dimakrok@bk.ru>
2020-03-01 16:09:13 +0300

    F thi S i'm out

--- lab_02/main.c
+++ lab_02/main.c
@@ -30,6 +30,13 @@ typedef struct{
     double *zArgs;
 }tableT;
 
+typedef struct{
+    int dotsNum;
+    double *xArgs;
+    double *yArgs;
+}tableOneDimT;
+
+
 // Функция чтения таблицы заданного формата
 int readfromTable(FILE *tableFile, tableT *table){
     if (!tableFile)
@@ -89,6 +96,18 @@ void freeTableT(tableT *table)
         free(table->zArgs);
 }
 
+int printTableOneDimT(tableOneDimT table)
+{
+#ifdef DEBUG
+    printf("%d", table.dotsNum);
+#endif
+
+    for (int i = 0; i < table.dotsNum; i++)
+        printf("|%+5lf|%+5lf|\n", *(table.xArgs + i), *(table.yArgs + i));
+
+    return 0;
+}
+
 double splitDiff(double yi, double yj, double xi, double xj){
 #ifdef DEBUG
     printf("\nyi=%lf yj=%lf xi=%lf xj=%lf\n", yi, yj,
@@ -97,7 +116,7 @@ double splitDiff(double yi, double yj, double xi, double xj){
     return (yi - yj)/(xi - xj);
 }
 
-void slowSortTable(tableT *table)
+void slowSortTable(tableOneDimT *table)
 {
     double memoryVar;
     for (int i = 0; i < table->dotsNum; i++){
@@ -115,7 +134,7 @@ void slowSortTable(tableT *table)
     }
 }
 
-int interpolationAlg(tableT *table, double findX, int polynomDegree){
+int interpolationAlg(tableOneDimT *table, double findX, int polynomDegree){
     if (polynomDegree <= 0)
         return ERROR;
     slowSortTable(table);
@@ -123,7 +142,7 @@ int interpolationAlg(tableT *table, double findX, int polynomDegree){
     printf("\nОтсортированная таблица значений будет иметь следующий вид:\n");
     printf("|%5s    |%5s    |\n", "x", "y");
     printf("|---------|---------|\n");
-    printTableT(*table);
+    printTableOneDimT(*table);
     printf("\n\n");
 
     // Нахождение позиции значения х в исходной таблице
@@ -228,168 +247,23 @@ int interpolationAlg(tableT *table, double findX, int polynomDegree){
     return 0;
 }
 
-// Алгоритм чистой воды, без предоставления всего и всея пользователю
-int interpNuPolAlg(tableT *table, double findX, int polynomDegree, double *foundY){
-    if (!table)
-        return ERROR;
-    slowSortTable(table);
+/*int multiInterpolationAlg(tableT *table, double findX, double findY, polynomDegreeX, int polynomDegreeY){
+    tableOneDimT curTable = { 0 };
 
-    if (!foundY)
-        return ERROR;
+    curTable.xArgs = calloc(sqrt(table->dotsNum), sizeof(double));
+    curTable.yArgs = calloc(sqrt(table->dotsNum), sizeof(double));
 
-    int position = -1;
-    for (int i = 0; i < table->dotsNum - 1; i++){
-        if (table->xArgs[i] - findX <= EPS && table->xArgs[i + 1] - findX >= EPS){
-            position = i;
-            break;
+    double *endMas = calloc(sqrt(table->dotsNum), sizeof(double));
+
+    for (int i = 0; i < sqrt(table->dotsNum); i++){
+        for (int j = 0; j < sqrt(table->dotsNum); j++){
+
         }
     }
-
-    if (position == -1){
-        return ERROR;
-    }
-    // Выделение узлов
-    if (table->dotsNum < polynomDegree + 1)
-        return ERROR;
-
-    int disDegree = polynomDegree, leftMove = 0, rightMove = 0;
-    double *nodesXMas = calloc(polynomDegree + 1, sizeof(double));
-    double *nodesYMas = calloc(polynomDegree + 1, sizeof(double));
-    while (disDegree > 0){
-        if (position - leftMove > 0){
-            leftMove++;
-            disDegree--;
-        }
-        if (position + rightMove < table->dotsNum - 1){
-            rightMove++;
-            disDegree--;
-        }
-    }
-    for (int i = position - leftMove, curNode = 0; i <= position + rightMove; i++, curNode++){
-        nodesYMas[curNode] = table->yArgs[i];
-        nodesXMas[curNode] = table->xArgs[i];
-    }
-
-    double **difMas = calloc(polynomDegree + 2, sizeof(double *));
-    difMas[0] = nodesXMas;
-    difMas[1] = nodesYMas;
-    nodesXMas = NULL;
-    nodesYMas = NULL;
-    // Расчёт таблицы с разделёнными разностями
-    for (int i = polynomDegree, curPos = 2; i > 0; i--, curPos++){
-        difMas[curPos] = calloc(i, sizeof(double));
-        for (int j = 0; j < i; j++)
-            difMas[curPos][j] = splitDiff(difMas[curPos - 1][j], difMas[curPos - 1][j + 1],
-                                          difMas[0][j], difMas[0][j - 1 + curPos]);
-    }
-
-    for (int i = 0; i < polynomDegree + 1; i++){
-        for (int j = 0; j < polynomDegree + 2 - i; j++)
-            if (difMas[j][i] == INFINITY || difMas[j][i] == -INFINITY){
-                for (int i = 0; i < polynomDegree + 2; i++)
-                    free(difMas[i]);
-                free(difMas);
-                return ERROR;
-            }
-    }
-
-    // Расчёт полинома
-    double finalPolynom = difMas[1][0], compGap = 1;
-    for (int i = 0; i < polynomDegree; i++)
-    {
-        for (int j = 0; j <= i; j++){
-            compGap *= findX - difMas[0][j];
-        }
-        compGap *= difMas[i + 2][0];
-        finalPolynom += compGap;
-        compGap = 1;
-    }
-
-    for (int i = 0; i < polynomDegree + 2; i++)
-        free(difMas[i]);
-    free(difMas);
-
-    *foundY = finalPolynom;
 
     return 0;
 }
-
-int halfAlg(tableT *table){
-    int position = -1;
-    for (int i = 0; i < table->dotsNum - 1; i++){
-        if ((table->yArgs[i] <= 0 && table->yArgs[i + 1] >= 0) || (table->yArgs[i] >= 0 && table->yArgs[i + 1] <= 0)){
-            position = i;
-            break;
-        }
-    }
-    if (position == -1){
-        printf("К несчастью, функция не имеет корней на заданном отрезке. Завершение программы...");
-        return ERROR;
-    }
-
-    printf("Обнаружен корень на отрезке: x = %.3lf; y = %.3lf -> x = %.3lf; y = %.3lf\n\n", table->xArgs[position],
-           table->yArgs[position], table->xArgs[position + 1], table->yArgs[position + 1]);
-
-    if (!table->yArgs[position] || !table->yArgs[position + 1]){
-        printf("Решение тривиально, ответ уже дан в таблице :(\n\n");
-        return 0;
-    }
-
-    double leftSide = table->xArgs[position], rightSide = table->xArgs[position + 1];
-    double newX, newY, rightY = table->yArgs[position + 1],
-            leftY = table->yArgs[position];
-    newX = (leftSide + rightSide) / 2;
-    interpNuPolAlg(table, newX, 2, &newY);
-    int helpErr = 0;
-    //while (fabs(leftY) > EPS && fabs(rightY) > EPS && helpErr < 1000){
-    while (fabs((leftSide - rightSide)/newX) > EPS){
-        newX = (leftSide + rightSide) / 2;
-        interpNuPolAlg(table, newX, 2, &newY);
-        if (newY * leftY < 0){
-            rightY = newY;
-            rightSide = newX;
-        }
-        else if (newY * rightY < 0){
-            leftY = newY;
-            leftSide = newX;
-        }
-        helpErr++;
-    }
-
-    if (helpErr == 100000){
-        printf("Задача не разрешилась за 100000 итераций.\n");
-        return 0;
-    }
-
-    if (fabs(leftY) < EPS)
-        newX = leftSide;
-    else if (fabs(rightY) < EPS)
-        newX = rightSide;
-
-    printf("Корень, определённый методом половинного деления на отрезке, равен: %lf", newX);
-
-    return 0;
-}
-
-int reverseInterpolationAlg(tableT *table, int polynomDegree){
-    printf("\n\nНайдём корень обратной интерполяцией.");
-
-    double *saveargs = table->xArgs;
-    table->xArgs = table->yArgs;
-    table->yArgs = saveargs;
-    saveargs = NULL;
-
-    printf("Поменяем местами значения ординат и абсцисс...\n");
-
-    printf("И зададим искомой величиной для поиска в алгоритме, предоставленном выше, нуль:\n\n");
-
-    int check = interpolationAlg(table, 0, polynomDegree);
-    if (check)
-        return ERROR;
-
-    return 0;
-}
-
+*/
 int main(){
     FILE *tableFile = fopen(FILE_NAME, "r");
     if (!tableFile){
@@ -416,11 +290,18 @@ int main(){
     int polynomDegreeX, polynomDegreeY;
     check = scanf("%d %d", &polynomDegreeX, &polynomDegreeY);
 
-    if (check != 1 || polynomDegreeX <= 0 || polynomDegreeY <= 0){
+    if (check != 2 || polynomDegreeX <= 0 || polynomDegreeY <= 0){
         return ERROR;
     }
-    /*
+
     check = 0;
+
+// РЕШЕНИЕ МОИХ ПРОБЕЛМ!!!
+    for (int i = 0; i < sqrt(table.dotsNum); i++){
+        printf("%lf ", *(table.xArgs + (int)(i*sqrt(table.dotsNum))));
+    }
+    /*
+    check = multiInterpolationAlg(&table, findX, findY, polynomDegreeX, polynomDegreeY);
 
     check = interpolationAlg(&table, findX, polynomDegree);
     if (check)
