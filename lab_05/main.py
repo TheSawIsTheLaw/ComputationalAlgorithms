8cca4eb46333341580ae07f4cc7d1cd3deaa92ee
xAMATEURx <dimakrok@bk.ru>
2020-05-09 16:48:04 +0300

    ADD: i love numpy...

--- lab_05/main.py
+++ lab_05/main.py
@@ -1,6 +1,17 @@
-from scipy.special import lpmn
+from numpy.polynomial.legendre import *
+from math import pi
 
 def main():
-    print(lpmn(5, 5, 1))
+    mNet = int(input("Set number M in netDif: "))
+    nNet = int(input("Set number N in netDif: "))
+
+    legArr = leggauss(mNet)
+    print("Legendre roots of polynom " + str(mNet) + " degree: ", end = ' ')
+    for i in legArr[0]: print(i, end=' ')
+    print()
+    print("A for Gauss: ", end = ' ')
+    for i in legArr[1]: print(i, end = ' ')
+
+
 
 main()
