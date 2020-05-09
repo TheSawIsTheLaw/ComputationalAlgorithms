b222d99c11b7a238160906a704fc20d15bf5513c
xAMATEURx <dimakrok@bk.ru>
2020-05-09 17:30:20 +0300

    ADD: read from table and table of values

--- lab_05/main.py
+++ lab_05/main.py
@@ -1,9 +1,23 @@
 from numpy.polynomial.legendre import *
 from math import pi
 
+def fillTableFromFile():
+    file = open("table.txt", 'r')
+    xCoordinates = list()
+    yCoordinates = list()
+    for line in file:
+        splited = line.split()
+        xCoordinates.append(float(splited[0]))
+        yCoordinates.append(float(splited[1]))
+    file.close()
+    return xCoordinates, yCoordinates
+
+
 def main():
     mNet = int(input("Set number M in netDif: "))
     nNet = int(input("Set number N in netDif: "))
+
+    table = fillTableFromFile()
 
     legArr = leggauss(mNet)
     print("Legendre roots of polynom " + str(mNet) + " degree: ", end = ' ')
@@ -11,6 +25,7 @@ def main():
     print()
     print("A for Gauss: ", end = ' ')
     for i in legArr[1]: print(i, end = ' ')
+
 
 
 
