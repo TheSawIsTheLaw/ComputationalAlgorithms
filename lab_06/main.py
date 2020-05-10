7b8247f2bdd88db50a35f50ad599ca54e010145a
xAMATEURx <dimakrok@bk.ru>
2020-05-10 17:59:36 +0300

    ADD: leftSide der was added

--- lab_06/main.py
+++ lab_06/main.py
@@ -1,8 +1,21 @@
 table = [[1, 2, 3, 4, 5, 6], [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]]
 # Абажаю хардкод)))))
 
+def leftSideDerivative(yValues, xValues):
+    answer = ['-', ]
+
+    for i in range(1, len(yValues)):
+        answer.append(yValues[i] - yValues[i - 1])
+
+    return answer
+
 def main():
-    print("Я у мамы путонщик")
+    yValues = table[1]
+    xValues = table[0]
+    firstColumn = leftSideDerivative(yValues, xValues)
+    print("LeftSideDerivative:", firstColumn)
+
+
 
 
 main()
