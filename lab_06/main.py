c5b3f3c62167f3cea61558b16835cc6b4686a1a3
xAMATEURx <dimakrok@bk.ru>
2020-05-10 18:05:04 +0300

    ADD: second method was added

--- lab_06/main.py
+++ lab_06/main.py
@@ -1,6 +1,7 @@
 table = [[1, 2, 3, 4, 5, 6], [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]]
 # Абажаю хардкод)))))
 
+
 def leftSideDerivative(yValues, xValues):
     answer = ['-', ]
 
@@ -9,13 +10,26 @@ def leftSideDerivative(yValues, xValues):
 
     return answer
 
+
+def centerDerivative(yValues, xValues):
+    answer = ['-']
+
+    for i in range(1, len(yValues) - 1):
+        answer.append((yValues[i + 1] - yValues[i - 1]) / 2)
+
+    answer.append('-')
+    return answer
+
+
 def main():
     yValues = table[1]
     xValues = table[0]
+
     firstColumn = leftSideDerivative(yValues, xValues)
     print("LeftSideDerivative:", firstColumn)
 
-
+    secondColumn = centerDerivative(yValues, xValues)
+    print("CenterDerivative:", secondColumn)
 
 
 main()
